07dddf91735f37a86630ee011f281da30d72154e
Olesia Dmytruk <oj.dmytruk@gmail.com>
2020-05-08 17:56:17 +0300

    Лабораторная работа 3, обработка заявок

---
+++ OS_lab3/Plots.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace OS_lab3
+{
+    partial class Plots
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(17, 56);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart2.Legends.Add(legend5);
+            this.chart2.Location = new System.Drawing.Point(348, 56);
+            this.chart2.Name = "chart2";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.chart2.Series.Add(series5);
+            this.chart2.Size = new System.Drawing.Size(300, 300);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart3.Legends.Add(legend6);
+            this.chart3.Location = new System.Drawing.Point(674, 56);
+            this.chart3.Name = "chart3";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chart3.Series.Add(series6);
+            this.chart3.Size = new System.Drawing.Size(300, 300);
+            this.chart3.TabIndex = 2;
+            this.chart3.Text = "chart3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(438, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 48);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Розпочати роботу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "залежність середнього часу очікування ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(213, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "від інтенсивності вхідного потоку заявок";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(391, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(202, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "залежність проценту простою ресурсу";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(391, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(213, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "від інтенсивності вхідного потоку заявок";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(723, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(193, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "залежність кількості заявок від часу";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(723, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(210, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "очікування при фіксованій інтенсивності";
+            // 
+            // Plots
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Name = "Plots";
+            this.Size = new System.Drawing.Size(986, 455);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ OS_lab3/Plots.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace OS_lab3
+{
+    public partial class Plots: UserControl
+    {
+        private Background controllerBackground;
+        private Foreground controllerForeground;
+
+        public Plots()
+        {
+            controllerBackground = new Background();
+            controllerForeground = new Foreground();
+            InitializeComponent();
+            controllerBackground.Tick = 500;
+            controllerForeground.Tick = 500;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double averageWait;
+            double procent;
+            List<Task> listOfTasks = new List<Task>();
+            List<Task> listOfTasks2 = new List<Task>();
+            List<Task> listOfTasks3 = new List<Task>();
+            List<Task> listOfTasks4 = new List<Task>();
+            var chartFirst = chart1.ChartAreas[0];
+            var chartSecond = chart2.ChartAreas[0];
+            var chartThird = chart3.ChartAreas[0];
+
+            chartFirst.AxisX.IntervalType = DateTimeIntervalType.Number;
+            chartSecond.AxisX.IntervalType = DateTimeIntervalType.Number;
+            chartThird.AxisX.IntervalType = DateTimeIntervalType.Number;
+
+            chartFirst.AxisX.LabelStyle.Format = "";
+            chartFirst.AxisY.LabelStyle.Format = "";
+            chartFirst.AxisX.Interval = 0.00007;
+            chartFirst.AxisY.Interval = 0.5;
+            chart1.Series.Add("залежність середнього часу очікування від інтенсивності вхідного потоку заявок");
+            chart1.Series["залежність середнього часу очікування від інтенсивності вхідного потоку заявок"].ChartType = SeriesChartType.Spline;
+            chart1.Series["залежність середнього часу очікування від інтенсивності вхідного потоку заявок"].Color = Color.Red;
+            chartSecond.AxisX.LabelStyle.Format = "";
+            chartSecond.AxisY.LabelStyle.Format = "";
+            chartSecond.AxisX.Interval = 0.0007;
+            chartSecond.AxisY.Interval = 0.05;
+            chart2.Series.Add("залежність проценту простою ресурсу від  інтенсивності вхідного потоку заявок");
+            chart2.Series["залежність проценту простою ресурсу від  інтенсивності вхідного потоку заявок"].ChartType = SeriesChartType.Spline;
+            chart2.Series["залежність проценту простою ресурсу від  інтенсивності вхідного потоку заявок"].Color = Color.Blue;
+            chartThird.AxisX.LabelStyle.Format = "";
+            chartThird.AxisY.LabelStyle.Format = "";
+            chartThird.AxisX.Interval = 0.01;
+            chartThird.AxisY.Interval = 0.1;
+            chart3.Series.Add("залежність кількості заявок від часу очікування при фіксованій  інтенсивності вхідного потоку заявок");
+            chart3.Series["залежність кількості заявок від часу очікування при фіксованій  інтенсивності вхідного потоку заявок"].ChartType = SeriesChartType.Spline;
+            chart3.Series["залежність кількості заявок від часу очікування при фіксованій  інтенсивності вхідного потоку заявок"].Color = Color.Green;
+
+            for (int i = 1; i<=32; i++ )
+            {
+                controllerBackground.FreqFrom = i;
+                controllerBackground.FreqTo = i;
+                controllerForeground.FreqFrom = i;
+                controllerForeground.FreqTo = i;
+                controllerBackground.MakeOneTick();
+                controllerForeground.MakeOneTick();
+                listOfTasks = controllerBackground.ListOfTasks;
+                listOfTasks2 = listOfTasks.Concat(controllerForeground.ListOfTasks).ToList();
+                averageWait = listOfTasks2.Average(task => task.Wait);
+                procent = ((double)listOfTasks2.FindAll(task => task.State == "виконано").Count / 500) * 100 ;
+                chart1.Series["залежність середнього часу очікування від інтенсивності вхідного потоку заявок"].Points.AddXY(500 / -i, averageWait);
+                chart2.Series["залежність проценту простою ресурсу від  інтенсивності вхідного потоку заявок"].Points.AddXY(500 / i, procent);
+            }
+            double averageWait2;
+
+            for (int i = 0; i < 32; i++)
+            {
+                controllerBackground.FreqFrom = 10;
+                controllerBackground.FreqTo = 10;
+                controllerForeground.FreqFrom = 10;
+                controllerForeground.FreqTo = 10;
+                controllerBackground.MakeOneTick();
+                controllerForeground.MakeOneTick();
+                listOfTasks3 = controllerBackground.ListOfTasks;
+                listOfTasks4 = listOfTasks3.Concat(controllerForeground.ListOfTasks).ToList();
+                averageWait2 = listOfTasks4.Average(task => task.Wait);
+                chart3.Series["залежність кількості заявок від часу очікування при фіксованій  інтенсивності вхідного потоку заявок"].Points.AddXY(averageWait2, listOfTasks4.FindAll(task => task.State == "виконано").Count);
+            }
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
